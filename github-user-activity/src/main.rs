59a875b0a12b220f48b4ef3e3c01512f53b83caa
yuvlian <yuvlian@naver.com>
2025-05-18 22:55:01 +0700

    github-user-activity | fine, something

--- github-user-activity/src/main.rs
+++ github-user-activity/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::collections::HashMap;
 
 mod models;
 use models::Events;
@@ -27,7 +28,7 @@ fn main() {
     };
 
     let rsp_reader = req.into_body().into_reader();
-    let rsp: Events = match serde_json::from_reader(rsp_reader) {
+    let events: Events = match serde_json::from_reader(rsp_reader) {
         Ok(v) => v,
         Err(e) => {
             eprintln!("{}", e);
@@ -35,6 +36,30 @@ fn main() {
         }
     };
 
-    // i aint doin allat
-    println!("{:#?}", rsp);
+    // Display the fetched activity in the terminal.
+
+    // Output:
+    // - Pushed 3 commits to kamranahmedse/developer-roadmap
+    // - Opened a new issue in kamranahmedse/developer-roadmap
+    // - Starred kamranahmedse/developer-roadmap
+    // - ...
+
+    let mut push_repo_map: HashMap<String, i64> = HashMap::with_capacity(events.len());
+    
+    for event in events {
+        if !(event.r#type == "PushEvent") {
+            continue;
+        }
+
+        if !(push_repo_map.contains_key(&event.repo.name)) {
+            push_repo_map.insert(event.repo.name, 1);
+        } else {
+            let r = push_repo_map.get_mut(&event.repo.name).unwrap();
+            *r = *r + 1
+        }
+    }
+
+    for (k, v) in push_repo_map.iter() {
+        println!("Pushed {} commits to {}", v, k);
+    }
 }
